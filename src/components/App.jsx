136f19c599a5cbbd831b8c515f0c9f5fed65a249
Michał Jarek <miciu77@gmail.com>
2023-01-20 17:21:12 +0100

    feeedback refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,82 +1,77 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-export class App extends Component {
-  static defaultProps = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  state = {
-    good: this.props.good,
-    neutral: this.props.neutral,
-    bad: this.props.bad,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   // ************************** Methods ********************************
 
-  handleIncrement = evt => {
+  const handleIncrement = evt => {
     const { name } = evt.target;
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }));
+    switch (name) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        console.log('Błąd: wybrano z poza zakresu.');
+    }
   };
 
-  countTotalFeedback = (option1, option2, option3) => {
+  const countTotalFeedback = (option1, option2, option3) => {
     return option1 + option2 + option3;
   };
 
-  countPositiveFeedbackPercentage = (positive, total) => {
+  const countPositiveFeedbackPercentage = (positive, total) => {
     return total > 0
       ? `${Number(Math.round((positive / total) * 100 + 'e+2') + 'e-2')}%`
       : 0;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const keys = Object.keys(this.state);
+  const keys = ['good', 'neutral', 'bad'];
+  let total = countTotalFeedback(good, neutral, bad);
+  let percentage = countPositiveFeedbackPercentage(good, total);
 
-    let total = this.countTotalFeedback(good, neutral, bad);
-    let percentage = this.countPositiveFeedbackPercentage(good, total);
-
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-start',
-          alignItems: 'flex-start',
-          fontSize: 40,
-          color: '#010101',
-          marginLeft: 20,
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={keys}
-            onLeaveFeedback={this.handleIncrement}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        fontSize: 40,
+        color: '#010101',
+        marginLeft: 20,
+      }}
+    >
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={keys} onLeaveFeedback={handleIncrement} />
+      </Section>
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={percentage}
           />
-        </Section>
-        <Section title="Statistics" >
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={percentage}
-            />
-          ) : (
-            <Notification message={'There is no feedback'} />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification message={'There is no feedback'} />
+        )}
+      </Section>
+    </div>
+  );
+};
